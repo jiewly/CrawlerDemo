2c5863383db8085b470f8ab7765c7c71b23afa12
Irin Krapookthong (NTL) <CF303520@tidlor.com>
2021-09-10 16:18:52 +0700

    init

---
+++ CrawlerDemo/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerDemo
+{
+    class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public string Price { get; set; }
+
+    }
+}
